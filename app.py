dafbc3ab20deb91efebd08faea384e9b771f90d9
Izz Hafeez <izzhafeez@gmail.com>
2024-04-04 12:07:21 +0800

    Exposing my privates

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import socket
 
 app = Flask(__name__)
 CORS(app)
-MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
+# MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
 
 @app.route("/")
 def hello_world():
@@ -24,7 +24,7 @@ def hello_world():
 def map_quiz(quiz_type, quiz_name):
   if request.method != 'POST':
     return 'Invalid request method', 405
-  connection = f"mongodb+srv://admin:{MONGO_PASSWORD}@cluster0.1jxisbd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsCAFile=isrgrootx1.pem"
+  connection = f"mongodb+srv://admin:{'di1ayXVEx7Gib0Do'}@cluster0.1jxisbd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsCAFile=isrgrootx1.pem"
   client = MongoClient(connection)
   name = request.json['name']
   score = request.json['score']
@@ -85,10 +85,9 @@ def map_quiz(quiz_type, quiz_name):
 
 @app.route('/api/<string:quiz_type>/<string:quiz_name>', methods=['GET'])
 def get_map_quiz(quiz_type, quiz_name):
-  print(quiz_type, quiz_name)
   if request.method != 'GET':
     return 'Invalid request method', 405
-  connection = f"mongodb+srv://admin:{MONGO_PASSWORD}@cluster0.1jxisbd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsCAFile=isrgrootx1.pem"
+  connection = f"mongodb+srv://admin:{'di1ayXVEx7Gib0Do'}@cluster0.1jxisbd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsCAFile=isrgrootx1.pem"
   client = MongoClient(connection)
   result = client.quiz[quiz_type].find_one({
     'quiz_name': quiz_name
